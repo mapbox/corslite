afb2c43f764ef637dca6585e3e96f9f6b64ddb77
Tom MacWright <tom@macwright.org>
2014-06-18 12:23:56 -0400

    Use support server

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ require('http').createServer(function (req, res) {
     var status = +req.url.substring(1);
     res.writeHead(status);
     res.end(status.toString());
-}).listen(+process.env.PORT);
+}).listen(process.env.ZUUL_PORT || +process.env.PORT);
